f9220a3286a93198eecefec96414d37bf1dd93ed
S.V.Subbarao.P <S.V.Subbarao.P@192.168.0.4>
2020-07-21 21:52:29 +0530

    Exceptions&Loggers

---
+++ exceptions_loggers/src/main/java/com/code/exceptions_loggers/ConstructionCost.java
@@ -0,0 +1,33 @@
+package com.code.exceptions_loggers;
+
+import java.util.logging.Logger;
+
+public class ConstructionCost 
+{
+ final static Logger logger = Logger.getLogger("ConstructionCost.java");
+ public double costForConstructionMaterial(String material,double area,boolean automation) 
+ {
+	if(material==null||area<=0||automation!=(true&&false))
+	{
+     logger.warning("material,area and automation can't be Null");
+	}
+
+	if(material.equals("Standard") && automation==false) 
+	{
+		return 1200*area;
+    }
+
+	else if(material.equals("AboveStandard") && automation==false) 
+	{
+		return 1500*area;
+    }
+    else if(material.equals("HighStandard") && automation==false) 
+    {
+        return 1800*area;
+    }
+   else 
+   {
+       return 2500*area;
+   }
+}
+}
